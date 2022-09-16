447e6633e3ae072a97c21b2e41e2acf63683c6a0
Richard <richardfarr7@gmail.com>
2022-09-16 11:53:05 -0700

    most files

---
+++ find.py
@@ -0,0 +1,11 @@
+import flask
+import sklearn
+import pandas
+import joblib
+# import gunicorn
+
+flask.__version__
+sklearn.__version__
+pandas.__version__
+joblib.__version__
+# gunicorn.__version__
